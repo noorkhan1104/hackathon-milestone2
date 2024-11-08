2dc3adcdede505cd9887b6821aedac22a09f3213
noorkhan1104 <noorussahar092@gmail.com>
2024-11-09 01:50:02 +0500

    first commit

---
+++ script.ts
@@ -0,0 +1,41 @@
+
+
+const Form = document.getElementById('resume-Form') as HTMLFormElement;
+Form.addEventListener ('submit' , (event: Event ) => {
+
+  const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
+  event.preventDefault();
+
+       //collect input values
+       const name=(document.getElementById('name')as HTMLInputElement).value
+       const email=(document.getElementById('email')as HTMLInputElement).value
+       const phone=(document.getElementById('tel')as HTMLInputElement).value
+       const education=(document.getElementById('education')as HTMLInputElement).value
+       const experience=(document.getElementById('experience')as HTMLInputElement).value
+       const skills=(document.getElementById('skills')as HTMLInputElement).value
+ 
+       //generate the resume content dynamically
+  const  resumeHTML = `
+  <h2><b>Resume<b></h2>
+  <h3>Personal Information</h3>
+  <p><b>Name:</b>${name}</p>
+  <p><b>Email:</b>${email}</p>
+  <p><b>Phone:</b>${phone}</p>
+
+  <h3>Education</h3>
+  <p>${education}</p>
+
+ <h3>Experience</h3>
+  <p>${experience}</p>
+
+ <h3>Skills</h3>
+  <p>${skills}</p>
+   `;
+
+//display the generated resume
+  if (resumeDisplayElement){
+    resumeDisplayElement.innerHTML =resumeHTML;
+  } else {
+    console.error('The Resume Display Element Is Missing');
+  }
+});
